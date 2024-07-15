1a1602a8c7d887fcd7c3b8da73b37736e33c0dab
lifanwu <keystoradio@gmail.com>
2024-07-15 16:55:37 -0400

    Update Maximize the Array Sum After Negating at Most K Elements.py

--- Maximize the Array Sum After Negating at Most K Elements.py
+++ Maximize the Array Sum After Negating at Most K Elements.py
@@ -43,3 +43,35 @@ A2 = [10, 9, 2, 3]
 print(max_negate_prefix_positive(A1))  # Output: [4, -1, -1, -1]
 print(max_negate_prefix_positive(A2))  # Output: [10, 9, -2, -3]
 # Time complexity of O(n^2)
+
+
+
+### Approach 2nd 
+# https://www.fastprep.io/problems/amazon-maximize-the-array-sum-after-negating-at-most-k-elements
+
+# from typing import List
+
+# #1.Compute the prefix sums of the array.
+# #2. Sort the prefix sums along with their original indices.
+# #3. Negate the smallest prefix sums as long as they don't make any prefix sum non-positive.
+
+# class Solution:
+#   def maxNegations(self, A: List[int]) -> int:
+#     s = sorted(A)
+#     maximum = s[-1]
+
+#     cnt = 0
+#     for head in s:
+#         maximum -= head
+#         if maximum > 0:
+#             cnt += 1
+#         else:
+#             return cnt
+
+# # Example usage:
+# A1 = [4, 1, 1, 1]
+# A2 = [10, 2, 3, 4]
+# ans = Solution()
+# print(ans.maxNegations(A1))  # Output: [4, -1, -1, -1]
+# print(ans.maxNegations(A2))  # Output: [10, 9, -2, -3]
+# # Time complexity of O(n^2)
